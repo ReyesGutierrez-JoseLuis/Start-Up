653b078fd60f39b8f396063b70834036006a9cfe
ReyesGutierrez <backtgh@gmail.com>
2021-10-14 17:53:55 -0500

    Commit

---
+++ src/API/howToUse.js
@@ -0,0 +1,19 @@
+const howToUseApp = [
+  {
+    id: 1,
+    title: "Sign in",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, Etquia quis?Lorem ipsum,  Etquia quis?",
+  },
+  {
+    id: 2,
+    title: "Customize your data",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 3,
+    title: "Improve your company's performance",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, consectetur adipisicing elit. Etquia quis?Lorem ipsum?",
+  },
+];
+
+export default howToUseApp;
---
+++ src/API/workApi.js
@@ -0,0 +1,22 @@
+const workapi = [
+  {
+    id: 1,
+    logo: "fas fa-download",
+    title: "Downlaod App",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+  },
+  {
+    id: 2,
+    logo: "fas fa-chalkboard-teacher",
+    title: "Customize your data ",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+  },
+  {
+    id: 3,
+    logo: "fas fa-donate",
+    title: "Improve your company's performance",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+  },
+];
+
+export default workapi;
